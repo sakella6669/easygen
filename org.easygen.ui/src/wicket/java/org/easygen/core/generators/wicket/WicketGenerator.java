d0a340113a2782ff11e190650f7540aa3b284c0e
manuel.eveno <manuel.eveno@ee5ff32e-2e33-0410-87c4-c16543ddb607>
2007-11-21 16:01:48 +0000

    Ajout des templates et du generateur pour Wicket

--- org.easygen.ui/src/wicket/java/org/easygen/core/generators/wicket/WicketGenerator.java
+++ org.easygen.ui/src/wicket/java/org/easygen/core/generators/wicket/WicketGenerator.java
@@ -1,6 +1,11 @@
 package org.easygen.core.generators.wicket;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.lang.WordUtils;
 import org.apache.log4j.Logger;
+import org.easygen.core.config.DataObject;
 import org.easygen.core.config.ProjectConfig;
 import org.easygen.core.generators.AbstractGenerator;
 import org.easygen.core.generators.GenerationException;
@@ -10,6 +15,9 @@ public class WicketGenerator extends AbstractGenerator {
 	private static final Logger logger = Logger.getLogger(WicketGenerator.class);
 	
 	protected static final String MODULE_NAME = "wicket";
+
+	private static final String PAGE_SUFFIX = "Page";
+	private static final String FORM_SUFFIX = "Form";
 
 	/**
 	 * @see org.easygen.core.generators.AbstractGenerator#getModuleName()
@@ -29,98 +37,87 @@ public class WicketGenerator extends AbstractGenerator {
 //		NavigationConfig navigationConfig = viewModuleConfig.getNavigationConfig();
 		String srcPath = projectConfig.getSrcPath();
 		String cfgPath = projectConfig.getCfgPath();
-        String webPath = projectConfig.getWebContentPath();
 		createPackagePath(srcPath, viewModuleConfig.getPackageName());
-		createPath(webPath, "WEB-INF");
-		createPath(webPath, "common");
-		createPath(cfgPath, "lang");
 
-//		// Variable qui précise si la couche service utilise Spring ou non
-//		boolean isSpringServiceModule = projectConfig.getServiceModuleNature().equals(SpringServiceModuleConfig.NATURE);
-//		context.put(IS_SPRING_SERVICE_MODULE, isSpringServiceModule);
-//
-//		context.put(IS_SITE_MESH_ENGINE, viewModuleConfig.isSiteMeshEngine());
-//		context.put(IS_TILES2_ENGINE, viewModuleConfig.isTiles2Engine());
-//
-//		context.put(NAVIGATION_CONFIG_KEY, navigationConfig);
-//
-//		copyStaticFiles(projectConfig);
-//
-//        String filename = createJavaFilename(viewModuleConfig, "GenericAction");
-//        generateFile("src/GenericAction.java.vm", srcPath + filename);
-//
-//        createPackagePath(srcPath, viewModuleConfig.getPackageName()+".util");
-//        filename = createJavaFilename(viewModuleConfig, "util/DisplayTagI18nWebwork2Adapter");
-//        generateFile("src/DisplayTagI18nWebwork2Adapter.java.vm", srcPath + filename);
-//
-//		List<String> classList = new LinkedList<String>();
-//		List<DataObject> objectList = new LinkedList<DataObject>();
-//		for (DataObject object : projectConfig.getObjects()) {
-//			if (object.isSelected() == false) {
-//				continue;
-//			}
-//			context.put(OBJECT, object);
-//
-//			filename = createFilename(viewModuleConfig, object.getClassName()+"Action-add-validation", XML_FILE_EXTENSION);
-//			generateFile("src/Action-add-validation.xml.vm", srcPath + filename);
-//			filename = createFilename(viewModuleConfig, object.getClassName()+"Action-update-validation", XML_FILE_EXTENSION);
-//			generateFile("src/Action-update-validation.xml.vm", srcPath + filename);
-//
-//			filename = createJavaFilename(viewModuleConfig, object.getClassName() + ACTION_SUFFIX);
-//			generateFile("src/Action.java.vm", srcPath + filename);
-//			
-//			String objectClassnameLowerCase = object.getClassName().toLowerCase();
-//			String jspPath = projectConfig.getWebContentPath() + objectClassnameLowerCase;
-//			createPath(jspPath);
-//
-//			generateFile("www/list.jsp.vm", jspPath + "/list.jsp");
-//			if (navigationConfig.isAddPage()) {
-//				generateFile("www/add.jsp.vm", jspPath + "/add.jsp");
-//			}
-//			if (navigationConfig.isViewPage()) {
-//				generateFile("www/view.jsp.vm", jspPath + "/view.jsp");
-//			}
-//			if (navigationConfig.isEditPage()) {
-//				generateFile("www/edit.jsp.vm", jspPath + "/edit.jsp");
-//			}
-//
-//			generateFile("src/lang/class.properties.vm", cfgPath + "lang/" + objectClassnameLowerCase + ".properties");
-//			generateFile("src/lang/class_fr.properties.vm", cfgPath + "lang/" + objectClassnameLowerCase + "_fr.properties");
-//
-//			context.remove(OBJECT);
-//			classList.add(object.getClassName());
-//			objectList.add(object);
-//		}
-//		
-//		context.put(CLASS_LIST, classList);
-//		generateFile("src/applicationContext-view.xml.vm", cfgPath + "applicationContext-view.xml");
-//		generateFile("src/struts.xml.vm", cfgPath + "struts.xml");
-//        generateFile("www/common/layout.jsp.vm", webPath + "common/layout.jsp");
-//		context.remove(CLASS_LIST);
-//
-//		generateFile("src/displaytag.properties.vm", cfgPath + "displaytag.properties");
-//
-//		generateFile("src/lang/common.properties.vm", cfgPath + "lang/common.properties");
-//		generateFile("src/lang/common_fr.properties.vm", cfgPath + "lang/common_fr.properties");
-//		
-//		generateFile("WEB-INF/web.xml.vm", webPath + "WEB-INF/web.xml");
-//        generateFile("www/index.jsp.vm", webPath + "index.jsp");
-//        generateFile("www/error.jsp.vm", webPath + "error.jsp");
+		copyStaticFiles(projectConfig);
+
+		generateFile("src/applicationContext-view.xml.vm", cfgPath + "applicationContext-view" + XML_FILE_EXTENSION);
+		generateFile("src/common/BaseForm.java.vm", srcPath + createJavaFilename(viewModuleConfig, "BaseForm"));
+		generateFile("src/common/IndexPage.java.vm", srcPath + createJavaFilename(viewModuleConfig, "IndexPage"));
+		generateFile("src/common/WebSession.java.vm", srcPath + createJavaFilename(viewModuleConfig, projectConfig.getName()+"WebSession"));
+		
+		createPackagePath(srcPath, viewModuleConfig.getPackageName()+".util");
+		generateFile("src/util/DirectPageLink.java.vm", srcPath + createJavaFilename(viewModuleConfig, "util/DirectPageLink"));
+		generateFile("src/util/LocalizerUtils.java.vm", srcPath + createJavaFilename(viewModuleConfig, "util/LocalizerUtils"));
+
+		List<String> classList = new LinkedList<String>();
+		for (DataObject object : projectConfig.getObjects()) {
+			if (object.isSelected() == false) {
+				continue;
+			}
+			context.put(OBJECT, object);
+			
+			WordUtils.swapCase(object.getClassName());
+			String objectPackage = Character.toLowerCase(object.getClassName().charAt(0))+object.getClassName().substring(1);
+
+			createPackagePath(srcPath, viewModuleConfig.getPackageName()+'.'+objectPackage);
+
+			String filename = createJavaFilename(viewModuleConfig, objectPackage+"/Add" + object.getClassName() + PAGE_SUFFIX);
+			generateFile("src/AddPage.java.vm", srcPath + filename);
+			filename = createJavaFilename(viewModuleConfig, objectPackage+"/Edit" + object.getClassName() + PAGE_SUFFIX);
+			generateFile("src/EditPage.java.vm", srcPath + filename);
+			filename = createJavaFilename(viewModuleConfig, objectPackage+'/'+object.getClassName() + "List" + PAGE_SUFFIX);
+			generateFile("src/ListPage.java.vm", srcPath + filename);
+			filename = createJavaFilename(viewModuleConfig, objectPackage+"/View" + object.getClassName() + PAGE_SUFFIX);
+			generateFile("src/ViewPage.java.vm", srcPath + filename);
+			filename = createJavaFilename(viewModuleConfig, objectPackage+'/'+object.getClassName() + FORM_SUFFIX);
+			generateFile("src/Form.java.vm", srcPath + filename);
+
+			createPackagePath(cfgPath, viewModuleConfig.getPackageName()+'.'+objectPackage);
+
+			filename = createFilename(viewModuleConfig, objectPackage+"/Add" + object.getClassName() + PAGE_SUFFIX, HTML_FILE_EXTENSION);
+			generateFile("www/AddPage.html.vm", cfgPath + filename);
+			filename = createFilename(viewModuleConfig, objectPackage+"/Edit" + object.getClassName() + PAGE_SUFFIX, HTML_FILE_EXTENSION);
+			generateFile("www/EditPage.html.vm", cfgPath + filename);
+			filename = createFilename(viewModuleConfig, objectPackage+"/View" + object.getClassName() + PAGE_SUFFIX, HTML_FILE_EXTENSION);
+			generateFile("www/ViewPage.html.vm", cfgPath + filename);
+			filename = createFilename(viewModuleConfig, objectPackage+'/'+object.getClassName() + "List" + PAGE_SUFFIX, HTML_FILE_EXTENSION);
+			generateFile("www/ListPage.html.vm", cfgPath + filename);
+
+			// TODO Add french translation
+			filename = createFilename(viewModuleConfig, objectPackage+'/'+object.getClassName() + "List" + PAGE_SUFFIX, PROPERTY_FILE_EXTENSION);
+			generateFile("www/ListPage.properties.vm", cfgPath + filename);
+
+			context.remove(OBJECT);
+			classList.add(object.getClassName());
+		}
+
+		context.put(CLASS_LIST, classList);
+		// TODO Add french translation
+		generateFile("www/common/BasePage.properties.vm", cfgPath + createFilename(viewModuleConfig, "BasePage", PROPERTY_FILE_EXTENSION));
+		generateFile("src/common/Application.java.vm", srcPath + createJavaFilename(viewModuleConfig, projectConfig.getName()+"Application"));
+		generateFile("src/common/BasePage.java.vm", srcPath + createJavaFilename(viewModuleConfig, "BasePage"));
+		context.remove(CLASS_LIST);
 	}
-	/**
-	 * @throws GenerationException 
-	 * 
-	 */
+	
 	protected void copyStaticFiles(ProjectConfig projectConfig) throws GenerationException {
 		logger.info("Génération des pages statiques de la couche view");
+		WicketModuleConfig viewModuleConfig = (WicketModuleConfig) projectConfig.getViewModuleConfig();
         String webPath = projectConfig.getWebContentPath();
+        String cfgPath = projectConfig.getCfgPath();
+        createPath(webPath, "WEB-INF");
+        createPath(webPath, "common");
+		createPackagePath(cfgPath, viewModuleConfig.getPackageName());
+
+        copyFile("www/web.xml", webPath + "WEB-INF/web.xml");
         copyFile("www/common/styles.css", webPath + "common/styles.css");
+        copyFile("www/common/BasePage.html", cfgPath + createFilename(viewModuleConfig, "BasePage", HTML_FILE_EXTENSION));
+        copyFile("www/common/IndexPage.html", cfgPath + createFilename(viewModuleConfig, "IndexPage", HTML_FILE_EXTENSION));
 	}
 	/**
 	 * @see org.easygen.core.generators.AbstractGenerator#postProcess(org.easygen.core.config.ProjectConfig)
 	 */
 	@Override
 	public void postProcess(ProjectConfig projectConfig) throws GenerationException {
-
 	}
 }
